903ea26b58ef6fce5f60bde4ce67c2da847baac6
anton rizk <rizk0020@algonquinlive.com>
2021-12-14 12:21:10 -0500

    pushing to ask questions

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const navContainer = document.querySelector('.nav-container')
 const settingbtn = document.getElementById('settings')
 const darkMode = document.getElementById('darkMode')
+const lightMode = document.getElementById('lightMode')
 const clockDisplay = document.getElementById('clockDisplay')
 const displayDay = document.getElementById('today-info')
 let $dayInfo = document.getElementById('dayInfo')
@@ -11,41 +12,40 @@ let greeting = document.getElementById('greeting')
 
 
 
-// api fetch 
+// api fetch need to fix it so it apears 
 
-// fetch('https://api.nasa.gov/planetary/apod?api_key=J3gluZeQZefozV6XxyZRyImJznmmoWCl1FF0iUgC')
-//     .then(function (response){
-//         return response.json()
-//     })
-//     .then(function (imageData){
-//         console.log(imageData)
+fetch('https://api.nasa.gov/planetary/apod?api_key=J3gluZeQZefozV6XxyZRyImJznmmoWCl1FF0iUgC&date=2021-12-13&thumbs=True')
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (imageData){
+        console.log(imageData)
 
-//         if (imageData.media_type === 'video'){
-//             document.querySelector('p').textContent = 'APOD is a video'
-//         } else {
-//             document.style.background = "url('https://api.nasa.gov/planetary/apod?api_key=J3gluZeQZefozV6XxyZRyImJznmmoWCl1FF0iUgC') no-repeat ";
-
-//             // document.querySelector('img').setAttribute('src', imageData.hdurl)
+        if (imageData.media_type === 'video'){
+            document.querySelector('p').textContent = 'APOD is a video'
+        } else {
             
-
-//         }
-//     })
+            document.querySelector('img').setAttribute('src', imageData.hdurl)
+            
+            // set attribute for background?
+            // document.querySelector('p').style(``) load in the image as  background it wont work so it wont be a background. 
+            
+            // anyway of fixing it
+        }
+    })
     
 
 
-
+// ======================
 // making the nav apear
+// ======================
 settingbtn.addEventListener('click', function(e){
    navContainer.classList.toggle('showNav')
-        })
+})
 
-    
-    
-    
-    
-    
-    
+// ======================================== 
 // setting up the clock with todays time
+// ======================================== 
 function clock(){
     const now = new Date()
     let hour = now.getHours()
@@ -53,10 +53,17 @@ function clock(){
     let second = now.getSeconds()
     theClock.textContent = hour + ':' + minute + ':' + second 
     setInterval(clock, 1000)
+
+    // adding if statment if the number is less then two digits add a 0 infront of it. 
+    // ===== not sure how to actually do this======
+    // if(minute > 10){
+    //     theClock =  '0' + minute
+    // }
 }
 clock()
-    
+// ============================================================
  // setting the time as a global thing to call the greeting
+//======================================== =======================
 const now = new Date()
 let hour = now.getHours()
 function greetings(){
@@ -75,8 +82,9 @@ function greetings(){
 }
 greetings()
 
-
+// ===========================================
 // set up event listener to display the time. 
+// ============================================
 displayDay.addEventListener('click', function(){
     let now = new Date()
     let year = now.getFullYear()
@@ -84,22 +92,35 @@ displayDay.addEventListener('click', function(){
     let day = now.getDate()
     $dayInfo.textContent = [year, month, day]
     // console.log(day)
+
+   
+})
+// another event listener that clears the text content with if statment . if exsit dont do anything if not then clear. 
+displayDay.addEventListener('click', function(e){
+    // struggling to add the event listener so when button is clicked again the info disappears
 })
 
 
 
 
-
+// adddin event listener so when this is clicked in the nav this styling appears. 
 clockDisplay.addEventListener('click', function(){
-    theClock.setAttribute('style', 'color:yellow;')
+    theClock.classList.add('neon')
+    // console.log(theClock)
+    // ====this event listner is not working and wont listen
 })
 
 
 // setting up the click event for displaying dark mode styles. 
-    darkMode.addEventListener('click', function(){
-        // document.getElementById('clock').textContent.style.color = 'white'
-        theClock.textContent.add = 'neon'
-        
+darkMode.addEventListener('click', function(){
+    let neonColor = $dayInfo
+    $dayInfo.classList.add('darkmode')
+    // ==== this is not working even console log wont pick it up not sure whats happening
 
+ })
+ lightMode.addEventListener('click', function(){
+    
+    $dayInfo.classList.add('darkmode')
+    
 
-    })
+ })
