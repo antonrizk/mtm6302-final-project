a75ae8a6732ad69b95a86ccd64343fa0fa27635a
anton rizk <rizk0020@algonquinlive.com>
2021-12-16 13:01:17 -0500

    final push- it is done. i tried my best

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-    
 const navContainer = document.querySelector('.nav-container')
 const settingbtn = document.getElementById('settings')
 const darkMode = document.getElementById('darkMode')
@@ -9,12 +7,35 @@ const displayDay = document.getElementById('today-info')
 let $dayInfo = document.getElementById('dayInfo')
 let theClock = document.getElementById('clock')
 let greeting = document.getElementById('greeting')
+// ===================================================
+//  setting up the greeting based on time of the day
+// ===================================================
+
+const now = new Date()
+let hour = now.getHours()
+function greetings(){
+
+    
+    if(hour <= 12){
+        greeting.innerHTML = 'Good morning'
+    }
+     if(hour >= 12){
+        greeting.innerHTML = 'Good afternoon'
+    }
+    if(hour >= 20){
+        greeting.innerHTML = 'Good Evening'
+    }
+   
+}
+greetings()
 
 
+// ================================================================================================
+// api fetch need to fix it so it apears (could not figure out how to make it apear on todays date)
+// =================================================================================================
 
-// api fetch need to fix it so it apears 
 
-fetch('https://api.nasa.gov/planetary/apod?api_key=J3gluZeQZefozV6XxyZRyImJznmmoWCl1FF0iUgC&date=2021-12-13&thumbs=True')
+fetch(`https://api.nasa.gov/planetary/apod?api_key=J3gluZeQZefozV6XxyZRyImJznmmoWCl1FF0iUgC&date&thumbs=True`)
     .then(function (response){
         return response.json()
     })
@@ -55,77 +76,56 @@ clock()
 setInterval(function() {
     clock()
 }, 1000);
-// ============================================================
- // setting the time as a global thing to call the greeting
-//======================================== =======================
-const now = new Date()
-let hour = now.getHours()
-function greetings(){
-
-    
-    if(hour <= 12){
-        greeting.innerHTML = 'Good morning'
-    }
-     if(hour >= 12){
-        greeting.innerHTML = 'Good afternoon'
-    }
-    if(hour >= 20){
-        greeting.innerHTML = 'Good Evening'
-    }
-   
-}
-greetings()
 
 // ===========================================
 // set up event listener to display the time. 
 // ============================================
-displayDay.addEventListener('click', function(){
+displayDay.addEventListener('click', function(e){
     let now = new Date()
     let year = now.getFullYear()
     let month = now.getMonth()+1
     let day = now.getDate()
-    $dayInfo.textContent = [year, month, day]
+    if($dayInfo.textContent = []){
+        $dayInfo.textContent = [year, month, day]
+    }
+    // else if ($dayInfo.textContent = []){
+    //     $dayInfo.textContent = []
+    // }
     // console.log(day)
 
    
 })
-// another event listener that clears the text content with if statment . if exsit dont do anything if not then clear. 
-displayDay.addEventListener('click', function(e){
-    // struggling to add the event listener so when button is clicked again the info disappears
-})
 
-
-
-
+// ================================================================================
 // adddin event listener so when this is clicked in the nav this styling appears. 
+// ================================================================================
 clockDisplay.addEventListener('click', function(e){
-   if(e.target = 'neon'){
-       theClock.classList.add('neon') 
-   }
-   else if (e.target = 'neon'){
-    theClock.classList.remove('neon') 
-   }
-   
-  
+    localStorage.setItem('neon', clockDisplay)
+   if(e.target = 'clockDisplay'){
+       theClock.classList.toggle('neon') 
+       
+   }  
 })
 
 
+// ================================================================================
 // setting up the click event for displaying dark mode styles. 
+// ================================================================================
 darkMode.addEventListener('click', function(){
+    localStorage.setItem('darkmode', darkMode)
+    $dayInfo.classList.toggle('darkmode')
     
-    $dayInfo.classList.add('darkmode')
-    // ==== this is not working even console log wont pick it up not sure whats happening
-
- })
-
- lightMode.addEventListener('click', function(){
     
-    $dayInfo.classList.add('lightmode')
+})
+// ================================================================================
+// setting up the click event for displaying light mode styles. 
+// ================================================================================
+lightMode.addEventListener('click', function(){
+    localStorage.setItem('lightmode', lightMode)
+    $dayInfo.classList.toggle('lightmode')
     
 
- })
+})
 
 
 
-//  local storage stuff
-// still trying to undertand local storage stuff i dont know why i left it till last minute.
